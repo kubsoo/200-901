e1d670993420790a7caa6420de9f1cba4abe1686
kubsoo <jakub.mazurkiewicz@gmail.com>
2020-01-05 23:32:52 +0100

    port changed from 8080 to 443

--- devnet_labs/dnav3-dnac-nbapi-modules/nbound_api.py
+++ devnet_labs/dnav3-dnac-nbapi-modules/nbound_api.py
@@ -4,7 +4,7 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 DNAC=os.environ.get('DNAC','sandboxdnac.cisco.com')
-DNAC_PORT=os.environ.get('DNAC_PORT',8080)
+DNAC_PORT=os.environ.get('DNAC_PORT',443)
 DNAC_USER=os.environ.get('DNAC_USER','devnetuser')
 DNAC_PASSWORD=os.environ.get('DNAC_PASSWORD','Cisco123!')
 
