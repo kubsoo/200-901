6c01abd5be3addd95c98d4965a26515e0aa6bb5a
kubsoo <jakub.mazurkiewicz@gmail.com>
2020-01-07 20:09:21 +0100

    commit

--- devnet_labs/path_trace/script.py
+++ devnet_labs/path_trace/script.py
@@ -1,1 +1,19 @@
 import requests
+from requests.auth import HTTPBasicAuth
+
+
+host = 'https://sandboxdnac.cisco.com'
+username = 'devnetuser'
+password = 'Cisco123!'
+api_version = 'v1'
+
+def get_token(host,username,password):
+    url = '{}/api/system/{}/auth/token'.format(host,api_version)
+    response = requests.post(url,auth=HTTPBasicAuth(username,password),verify=False)
+    token = response.json()['Token']
+
+    return token
+
+if __name__ == "__main__":
+    token = get_token(host,username,password)
+    print(token)
