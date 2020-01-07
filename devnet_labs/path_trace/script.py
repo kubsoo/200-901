fbec139a9916730f2a0998b829bfbfed84623e38
kubsoo <jakub.mazurkiewicz@gmail.com>
2020-01-07 20:53:32 +0100

    commit

--- devnet_labs/path_trace/script.py
+++ devnet_labs/path_trace/script.py
@@ -8,15 +8,26 @@ username = 'devnetuser'
 password = 'Cisco123!'
 api_version = 'v1'
 
-def get_token(host,username,password):
+def get_token(host,username,password,api_version):
     url = '{}/api/system/{}/auth/token'.format(host,api_version)
     response = requests.post(url,auth=HTTPBasicAuth(username,password),verify=False)
     token = response.json()['Token']
 
     return token
 
+
+def get_host(host,token,api_version):
+    url = '{}/api/{}/host'.format(host,api_version)
+    headers = {
+        "x-auth-token":token
+    }
+    response = requests.get(url,headers=headers,verify=False)
+    print(response.text)
+
+    return response
+
 if __name__ == "__main__":
-    token = get_token(host,username,password)
+    token = get_token(host,username,password,api_version)
     print(token)
     parser = argparse.ArgumentParser()
     parser.add_argument("source_ip", help = "Source IP Address")
@@ -31,3 +42,5 @@ if __name__ == "__main__":
 
     print(source_ip,destination_ip)
 
+
+    get_host(host,token,api_version)
